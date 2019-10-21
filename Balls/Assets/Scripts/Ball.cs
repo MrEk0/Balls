aeac7bc6e00539b73ac0836c5a1602b3b4ac77ce
MrEk0 <51297787+MrEk0@users.noreply.github.com>
2019-10-21 12:47:38 +0300

    ball
    script obj

---
+++ Balls/Assets/Scripts/Ball.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="New Ball", menuName ="Ball")]
+public class Ball : ScriptableObject
+{
+    public float speed;
+    public float pointsForDestroy;
+    public Sprite sprite;
+}
