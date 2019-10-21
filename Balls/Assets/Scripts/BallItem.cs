2b27fa7778e288570afaf1529afa156a3b2251ea
MrEk0 <51297787+MrEk0@users.noreply.github.com>
2019-10-21 12:46:40 +0300

    destroyer

---
+++ Balls/Assets/Scripts/BallItem.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class BallItem : MonoBehaviour
+{
+    [SerializeField] Ball ball;
+    
+    private float pointsForDestroy;
+
+    private Sprite sprite;
+    private Rigidbody2D rd;
+    private Vector2 speed;
+
+    public float startMultiplier {private get; set; } = 1f;
+    public event Action<float> OnTouch;
+
+    private void OnEnable()
+    {
+        GameManager.instance.OnTimeDown += SetSpeed;
+    }
+
+    private void OnDisable()
+    {
+        GameManager.instance.OnTimeDown -= SetSpeed;
+    }
+
+    private void Awake()
+    {
+        pointsForDestroy = ball.pointsForDestroy;
+        GetComponent<SpriteRenderer>().sprite = ball.sprite;
+        rd = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        speed = new Vector2(0, ball.speed * startMultiplier);
+    }
+
+    private void FixedUpdate()
+    {
+        rd.velocity = speed;
+    }
+
+    private void OnMouseDown()
+    {
+        OnTouch(pointsForDestroy);
+        Destroy(gameObject);
+    }
+
+    public void SetSpeed(float multiplier)
+    {
+        float newSpeed = speed.y * multiplier;
+        speed = new Vector2(0, newSpeed);
+    }
+}
